30754ad3cd8466970f4bf5093112482b20581206
AshishPython11 <ashishpython11@gmail.com>
2024-11-29 18:03:19 +0530

    Test cases added
    Test cases added

--- src/Pages/PDFList/PDFList.tsx
+++ src/Pages/PDFList/PDFList.tsx
@@ -98,12 +98,12 @@ const PDFList = () => {
       getData(
         `https://uatllm.gyansetu.ai/display-files?class_name=${selectedClass}`
       )
-        .then((response: any) => {
+        ?.then((response: any) => {
           if (response.status === 200) {
             setFileList(response?.filenames);
           }
         })
-        .catch((error) => {
+        ?.catch((error) => {
           toast.error(error?.message, {
             hideProgressBar: true,
             theme: "colored",
@@ -147,7 +147,7 @@ const PDFList = () => {
     console.log("Delete File", selectedFile);
 
     const payload = {
-      file_id:selectedFile?.pdf_id,
+      file_id: selectedFile?.pdf_id,
       file_path: selectedFile?.pdf_path,
       file_name: selectedFile?.pdf_file_name,
       class_name: selectedClass,
@@ -179,226 +179,146 @@ const PDFList = () => {
       {loading && <FullScreenLoader />}
       <div className="main-wrapper">
         <div className="main-content">
-        <div className="card">
-          <div className="card-body">
-            <div className="table_wrapper">
-              <div className="table_inner">
-                <div
-                  className="containerbutton"
-                  style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                  }}
-                >
-                  <Typography variant="h6" sx={{ m: 1 }}>
-                    {/* <div className='main_title'>Teacher</div> */}
-                  </Typography>
-                </div>
-                <div
-                  style={{
-                    display: "flex",
-                    gap: "20px",
-                    marginBottom: "20px",
-                    alignItems: "center",
-                  }}
-                >
-                  <div>
-                    <FormControl sx={{ minWidth: 300 }}>
-                      <InputLabel
-                        id="select-class-label"
-                        sx={{ color: inputfieldtext(namecolor) }}
-                      >
-                        Class *
-                      </InputLabel>
-                      <Select
-                        labelId="select-class-label"
-                        value={selectedClass}
-                        onChange={handleChange}
-                        label="Class *"
-                        placeholder="Select class"
-                        variant="outlined"
-                        name="class_id"
-                        sx={{
-                          backgroundColor: inputfield(namecolor),
-                          color: inputfieldtext(namecolor),
-                        }}
-                        MenuProps={{
-                          PaperProps: {
-                            style: {
-                              backgroundColor: inputfield(namecolor),
-                              color: inputfieldtext(namecolor),
-                            },
-                          },
-                        }}
-                      >
-                        {classes?.map((classes) => (
-                          <MenuItem
-                            key={classes.class_name}
-                            value={classes.class_name}
-                            sx={{
-                              backgroundColor: inputfield(namecolor),
-                              color: inputfieldtext(namecolor),
-                              "&:hover": {
-                                backgroundColor: inputfieldhover(namecolor), // Change this to your desired hover background color
-                              },
-                            }}
-                          >
-                            {classes?.new_class_name}
-                          </MenuItem>
-                        ))}
-                      </Select>
-                    </FormControl>
-                  </div>
-                  {/* <div>
-                    <FormControl sx={{ minWidth: 300 }}>
-                      <InputLabel
-                        id="select-file-label"
-                        sx={{ color: inputfieldtext(namecolor) }}
-                      >
-                        File *
-                      </InputLabel>
-                      <Select
-                        labelId="select-file-label"
-                        value={selectedFile}
-                        disabled={!selectedClass}
-                        onChange={handleChange}
-                        label="File *"
-                        placeholder="Select file"
-                        variant="outlined"
-                        name={"file_id"}
-                        sx={{
-                          backgroundColor: inputfield(namecolor),
-                          color: inputfieldtext(namecolor),
-                        }}
-                        MenuProps={{
-                          PaperProps: {
-                            style: {
-                              backgroundColor: inputfield(namecolor),
-                              color: inputfieldtext(namecolor),
-                            },
-                          },
-                        }}
-                      >
-                        {fileList?.map((file) => (
-                          <MenuItem
-                            key={file.pdf_path}
-                            value={file.pdf_path}
-                            sx={{
-                              backgroundColor: inputfield(namecolor),
-                              color: inputfieldtext(namecolor),
-                              "&:hover": {
-                                backgroundColor: inputfieldhover(namecolor), // Change this to your desired hover background color
-                              },
-                            }}
-                          >
-                            {file?.pdf_file_name}
-                          </MenuItem>
-                        ))}
-                      </Select>
-                    </FormControl>
-                  </div>
-                  <div>
-                    <a
-                      href={
-                        selectedFile.length > 0
-                          ? `http://13.232.96.204:5000/files${selectedFile}`
-                          : ""
-                      }
-                      target={selectedFile.length > 0 ? "_blank" : ""}
-                    >
-                      <Button
-                        variant="contained"
-                        component="label"
-                        className={`custom-button ${
-                          !selectedFile ? "disabled-mainbutton" : "mainbutton"
-                        }`}
-                        disabled={!selectedFile}
-                      >
-                        Preview
-                      </Button>
-                    </a>
-                  </div> */}
-                  {/* <div>
-                    <Button
-                      variant="contained"
-                      component="label"
-                      className={`custom-button ${
-                        !selectedFile ? "disabled-mainbutton" : "mainbutton"
-                      }`}
-                      disabled={!selectedFile}
-                      onClick={handleDelete}
-                    >
-                      Delete
-                    </Button>
-                  </div> */}
-                </div>
-                <Box marginTop="10px">
-                  <MaterialReactTable
-                    columns={columns}
-                    data={fileList}
-                    enableRowVirtualization
-                    positionActionsColumn="first"
-                    muiTablePaperProps={{
-                      elevation: 0,
+          <div className="card">
+            <div className="card-body">
+              <div className="table_wrapper">
+                <div className="table_inner">
+                  <div
+                    className="containerbutton"
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
                     }}
-                    enableRowActions
-                    displayColumnDefOptions={{
-                      "mrt-row-actions": {
-                        header: "Actions",
-                        size: 150,
-                      },
+                  >
+                    <Typography variant="h6" sx={{ m: 1 }}>
+                      {/* <div className='main_title'>Teacher</div> */}
+                    </Typography>
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      gap: "20px",
+                      marginBottom: "20px",
+                      alignItems: "center",
                     }}
-                    renderRowActions={(row) => (
-                      <Box
-                        sx={{
-                          display: "flex",
-                          flexWrap: "nowrap",
-                          gap: "0.5",
-                          marginLeft: "-5px",
-                          width: "140px",
-                        }}
-                      >
-                        <Tooltip arrow placement="bottom" title="View">
-                          <a
-                            href={`https://uatllm.gyansetu.ai/files/${row?.row?.original?.pdf_path}`}
-                            target="_blank" rel="noreferrer"
-                          >
+                  >
+                    <div>
+                      <FormControl sx={{ minWidth: 300 }}>
+                        <InputLabel
+                          data-testid="class_text"
+                          id="select-class-label"
+                          sx={{ color: inputfieldtext(namecolor) }}
+                        >
+                          Class *
+                        </InputLabel>
+                        <Select
+                        data-testid="class_text_input"
+                          labelId="select-class-label"
+                          value={selectedClass}
+                          onChange={handleChange}
+                          label="Class *"
+                          placeholder="Select class"
+                          variant="outlined"
+                          name="class_id"
+                          sx={{
+                            backgroundColor: inputfield(namecolor),
+                            color: inputfieldtext(namecolor),
+                          }}
+                          MenuProps={{
+                            PaperProps: {
+                              style: {
+                                backgroundColor: inputfield(namecolor),
+                                color: inputfieldtext(namecolor),
+                              },
+                            },
+                          }}
+                        >
+                          {classes?.map((classes) => (
+                            <MenuItem
+                              key={classes.class_name}
+                              value={classes.class_name}
+                              sx={{
+                                backgroundColor: inputfield(namecolor),
+                                color: inputfieldtext(namecolor),
+                                "&:hover": {
+                                  backgroundColor: inputfieldhover(namecolor), // Change this to your desired hover background color
+                                },
+                              }}
+                            >
+                              {classes?.new_class_name}
+                            </MenuItem>
+                          ))}
+                        </Select>
+                      </FormControl>
+                    </div>
+                  </div>
+                  <Box marginTop="10px">
+                    <MaterialReactTable
+                      columns={columns}
+                      data={fileList}
+                      enableRowVirtualization
+                      positionActionsColumn="first"
+                      muiTablePaperProps={{
+                        elevation: 0,
+                      }}
+                      enableRowActions
+                      displayColumnDefOptions={{
+                        "mrt-row-actions": {
+                          header: "Actions",
+                          size: 150,
+                        },
+                      }}
+                      renderRowActions={(row) => (
+                        <Box
+                          sx={{
+                            display: "flex",
+                            flexWrap: "nowrap",
+                            gap: "0.5",
+                            marginLeft: "-5px",
+                            width: "140px",
+                          }}
+                        >
+                          <Tooltip arrow placement="bottom" title="View">
+                            <a
+                              href={`https://uatllm.gyansetu.ai/files/${row?.row?.original?.pdf_path}`}
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              <IconButton
+                                sx={{
+                                  width: "35px",
+                                  height: "35px",
+                                  color: tabletools(namecolor),
+                                }}
+                              >
+                                <VisibilityIcon />
+                              </IconButton>
+                            </a>
+                          </Tooltip>
+                          <Tooltip arrow placement="bottom" title="Delete">
                             <IconButton
                               sx={{
                                 width: "35px",
                                 height: "35px",
                                 color: tabletools(namecolor),
                               }}
+                              onClick={() => {
+                                handleDeleteFiles(row?.row?.original);
+                              }}
                             >
-                              <VisibilityIcon />
+                              <TrashIcon />
                             </IconButton>
-                          </a>
-                        </Tooltip>
-                        <Tooltip arrow placement="bottom" title="Delete">
-                          <IconButton
-                            sx={{
-                              width: "35px",
-                              height: "35px",
-                              color: tabletools(namecolor),
-                            }}
-                            onClick={() => {
-                              handleDeleteFiles(row?.row?.original);
-                            }}
-                          >
-                            <TrashIcon />
-                          </IconButton>
-                        </Tooltip>
-                      </Box>
-                    )}
-                  />
-                </Box>
+                          </Tooltip>
+                        </Box>
+                      )}
+                    />
+                  </Box>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        </div>
-        
       </div>
       <DeleteDialog
         isOpen={dataDelete}
