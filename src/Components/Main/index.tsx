f178f4b5d7b26b54dc6bb2c05c407ac04fae46db
AshishPython11 <ashishpython11@gmail.com>
2024-12-07 15:24:19 +0530

    Removed commented lines

--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -2,11 +2,7 @@ import React, { useEffect } from "react";
 import Header from "../Header/index";
 import Footer from "../Footer/index";
 import Sidebar from "../Sidebar/index";
-// import "../Main/Main.scss";
 import { Outlet } from "react-router-dom";
-// import "../../assets/css/main.min.css";
-// import "../../assets/css/newstyle.min.css";
-// import "../../assets/css/newstyle.scss";
 
 const Main = () => {
   const synth: SpeechSynthesis = window?.speechSynthesis;
@@ -20,14 +16,8 @@ const Main = () => {
 
         <div className="main_section" id="main-content">
           <Header />
-          {/* <div className="sidebar_section"> */}
           <Sidebar />
-          {/* </div> */}
-          {/* <div className="dashboard_main_content"> */}
           <Outlet />
-          {/* </div> */}
-          {/* <div className="footer"> */}
-          {/* </div> */}
         </div>
       </div>
       <Footer />
