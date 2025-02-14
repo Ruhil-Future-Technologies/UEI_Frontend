57cdbf31286fda4acb71cda571af9ad56a134171
AshishPython12 <ashish.mishra@rftsystems.com>
2025-02-14 11:21:42 +0530

    base URL change for Production env
    base URL change for Production env

--- src/utils/http-client.tsx
+++ src/utils/http-client.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const httpClient = axios.create({
-  baseURL: 'https://uatapi.gyansetu.ai/',
+  baseURL: 'https://api.gyansetu.ai/',
   // baseURL: "http://127.0.0.1:5000/" || "http://localhost:3000",
 
   headers: {
