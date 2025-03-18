3cb266b27b05891a18e7a898f129acc09d3fc6cf
rahul-gyansetu <rahul.dev@rftsystems.com>
2025-03-18 11:57:34 +0530

    bugs fixes

--- src/utils/http-client.tsx
+++ src/utils/http-client.tsx
@@ -4,7 +4,7 @@ export const httpClient = axios.create({
   // baseURL: 'http://13.233.97.160:5000/',
   //baseURL: 'http://65.2.179.133:5000/',
   // baseURL: "http://127.0.0.1:5000/" || "http://localhost:3000",
-  baseURL: 'http://127.0.0.1:5000/',
+  baseURL: 'https://qaapi.gyansetu.ai/',
 
   headers: {
     'Content-Type': 'application/json',
