ef84b42f04984a5351797314c66dbbf2582c2e5d
AshishPython12 <ashish.mishra@rftsystems.com>
2025-04-15 13:29:23 +0530

    minor changes for when we change academic history it not change subject preference filed class to semester (#168)
    minor changes for when we change academic history it not change subject preference filed class to semester

--- src/Pages/StudentSubjectPreference/index.tsx
+++ src/Pages/StudentSubjectPreference/index.tsx
@@ -363,6 +363,7 @@ const StudentSubjectPreference: React.FC<PropsItem> = ({
           } else {
             course_id=response?.data[0]?.course_id;
             semester_id=response?.data[0]?.sem_id;
+            setAcademic(false);
             await getData('college_subject/list')
               .then((data: any) => {
                 if (data.status) {
