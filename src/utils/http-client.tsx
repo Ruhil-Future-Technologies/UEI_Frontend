b5b27470d8708bbe22f528329579967e5a839c27
rahul-gyansetu <rahul.dev@rftsystems.com>
2025-04-23 15:04:46 +0530

    UN-1663 fixed

--- src/utils/http-client.tsx
+++ src/utils/http-client.tsx
@@ -1,14 +1,10 @@
 import axios from 'axios';
 
 export const httpClient = axios.create({
-
-    //  baseURL: 'https://uatapi.gyansetu.ai/',
+  //  baseURL: 'https://uatapi.gyansetu.ai/',
   // baseURL: "http://127.0.0.1:5000/" || "http://localhost:3000",
 
-  baseURL: 'https://qaapi.gyansetu.ai/',
-
-
-
+  baseURL: 'http://127.0.0.1:5000/',
 
   headers: {
     'Content-Type': 'application/json',
