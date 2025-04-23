a1f58a493de3f1a782bace8ad85265f317ca9b06
rahul-gyansetu <rahul.dev@rftsystems.com>
2025-04-23 15:07:02 +0530

    api url fix

--- src/utils/http-client.tsx
+++ src/utils/http-client.tsx
@@ -4,7 +4,7 @@ export const httpClient = axios.create({
   //  baseURL: 'https://uatapi.gyansetu.ai/',
   // baseURL: "http://127.0.0.1:5000/" || "http://localhost:3000",
 
-  baseURL: 'http://127.0.0.1:5000/',
+  baseURL: 'https://qaapi.gyansetu.ai/',
 
   headers: {
     'Content-Type': 'application/json',
