f0bb84f4201037cab1e611a0ff445da4bd55ccdf
AshishPython12 <ashish.mishra@rftsystems.com>
2025-04-25 18:14:40 +0530

    change dynamic URL based on all env
    change dynamic URL based on all env

--- src/Pages/Teacher/Assignments/CreateAssignments.tsx
+++ src/Pages/Teacher/Assignments/CreateAssignments.tsx
@@ -63,7 +63,6 @@ import dayjs, { Dayjs } from 'dayjs';
 import { Autocomplete, Chip } from '@mui/material';
 import ReactQuill from 'react-quill';
 import QuizModal from './QuizModal';
-import axios from 'axios';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker/DateTimePicker';
 import FullScreenLoader from '../../Loader/FullScreenLoader';
 import AssignmentModal from './AssignmentModal';
@@ -1251,14 +1250,9 @@ export const CreateAssignments = () => {
     try {
       if (type == 'assignment') {
         setLoading(true);
-        const response = await axios.post(
+        const response = await postData(
           ASSIGNMENT.GENERATE_AI_ASSIGNMENT,
           payload,
-          {
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          },
         );
         setAssignmentGenrData(response?.data);
 
@@ -1266,7 +1260,7 @@ export const CreateAssignments = () => {
         setLoading(false);
       } else {
         setLoading(true);
-        const response = await axios.post(GENERATE_QUIZ, payload);
+        const response = await postData(GENERATE_QUIZ, payload);
         setQuizData(response?.data);
         setIsModalOpen(true);
         setLoading(false);
