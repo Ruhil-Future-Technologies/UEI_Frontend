c9180f0032e9f4fb2d866534221409aa8959996d
rahul-gyansetu <rahul.dev@rftsystems.com>
2025-04-28 13:10:40 +0530

    parent pages added

---
+++ src/Pages/Parent/ParentDashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ParentDashboard = () => {
+  return (
+    <div className="main-wrapper">
+      <div className="main-content">Parent Dashboard test</div>
+    </div>
+  );
+};
+
+export default ParentDashboard;
