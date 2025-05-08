1b418044e5c4ffbb8fb9707c69ac3b1b51a36266
rahul-gyansetu <rahul.dev@rftsystems.com>
2025-05-08 10:49:59 +0530

    UN-1443 completed

--- src/Components/Chart/StudentChart.tsx
+++ src/Components/Chart/StudentChart.tsx
@@ -121,7 +121,7 @@ const StudentDashboardCharts = () => {
               if (userdata?.entity_name === 'college') {
                 const subjectMap: any = {};
 
-                collegeData.data.subjects_data.forEach((subject: any) => {
+                collegeData?.data?.subjects_data?.forEach((subject: any) => {
                   subjectMap[subject.subject_name] = subject.subject_name;
                 });
 
@@ -167,8 +167,8 @@ const StudentDashboardCharts = () => {
               } else {
                 const subjectMap: any = {};
 
-                schoolData?.data?.subjects_data?.forEach((subject: any) => {
-                  subjectMap[subject?.subject_name] = subject?.subject_name;
+                schoolData.data.subjects_data.forEach((subject: any) => {
+                  subjectMap[subject.subject_name] = subject.subject_name;
                 });
 
                 const labels: any = [];
